30d6f1d976cb0ac60f4d0ec4903650e55a86b818
Liz Liu <109649330+LizLiu0930@users.noreply.github.com>
2022-07-29 15:34:28 +0800

    Add files via upload

---
+++ industry-lab-control-flow-0725-LizLiu0930/src/ictgradschool/industry/controlflow/coderunner/AdvancedCodeRunner.java
@@ -0,0 +1,44 @@
+package ictgradschool.industry.controlflow.coderunner;
+
+/**
+ * Please run TestCodeRunner to check your answers
+ */
+public class AdvancedCodeRunner {
+
+    /**
+     * Q1. When given an integer, return an integer that is the reverse (its numbers are in reverse to the input order).
+     *
+     * @param number
+     * @return the integer with digits in reverse order
+     */
+    public int reverseInt(int number) {
+        int reverseNum = 0;
+        // TODO write answer for Q1
+        return reverseNum;
+    }
+
+
+    /**
+     * Q2. Determine if the given integer is a palindrome (ignoring negative sign).
+     *
+     * @param num
+     * @return true is int is palindrome, false otherwise
+     */
+    public boolean isIntPalindrome(int num) {
+        // TODO write answer for Q2
+        return false;
+    }
+
+
+    /**
+     * Q3. Determines the Excel column name of the given column number.
+     *
+     * @param num
+     * @return the column title as a String
+     */
+    public String convertIntToColTitle(int num) {
+        String columnName = "";
+        // TODO write answer for Q3
+        return columnName;
+    }
+}
